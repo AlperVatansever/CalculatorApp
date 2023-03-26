31beed4671f5301f559a1fb2bf1ea4aeb5c24c24
AlperVatansever <alpervat@gmail.com>
2023-03-26 12:32:33 +0300

    feat:add inputs

--- Program.cs
+++ Program.cs
@@ -5,6 +5,10 @@
         static void Main(string[] args)
         {
             Console.WriteLine("Hesap Makinesi\n");
+            Console.Write("işlem yapmak istediğiniz 1. sayıyı giriniz: ");
+            double sayi1=double.Parse(Console.ReadLine());           
+            Console.Write("işlem yapmak istediğiniz 2. sayıyı giriniz: ");
+            double sayi2=double.Parse(Console.ReadLine());
         }
     }
 }
