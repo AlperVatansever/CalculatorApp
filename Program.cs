e42b9172d64cd34a6dfd19cbd7fc391a6d225cce
AlperVatansever <alpervat@gmail.com>
2023-03-26 12:57:29 +0300

    refac: organise code

--- Program.cs
+++ Program.cs
@@ -14,7 +14,8 @@
             double sonuc = 0;
             switch(islem)
             {
-                case "+": sonuc = sayi1 + sayi2;
+                case "+": 
+                    sonuc = sayi1 + sayi2;
                     break;
                 case "-":
                     sonuc = sayi1 - sayi2;
@@ -24,6 +25,9 @@
                     break;
                 case "/":
                     sonuc = sayi1 / sayi2;
+                    break;
+                case "%":
+                    sonuc = sayi1 % sayi2;
                     break;
             }
             Console.WriteLine("{0}{1}{2}={3}",sayi1,islem,sayi2,sonuc);
