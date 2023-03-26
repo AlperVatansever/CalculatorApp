b54cac296090c96cc65a387683c31a91eaf3a55f
AlperVatansever <alpervat@gmail.com>
2023-03-26 12:37:26 +0300

    fix: add input which is forgetten

--- Program.cs
+++ Program.cs
@@ -6,9 +6,12 @@
         {
             Console.WriteLine("Hesap Makinesi\n");
             Console.Write("işlem yapmak istediğiniz 1. sayıyı giriniz: ");
-            double sayi1=double.Parse(Console.ReadLine());           
+            double sayi1=double.Parse(Console.ReadLine());
+            Console.Write("yapmak istediğiniz işlemi seçiniz(+),(-),(/),(*),(%): ");
+            string islem=Console.ReadLine();    
             Console.Write("işlem yapmak istediğiniz 2. sayıyı giriniz: ");
             double sayi2=double.Parse(Console.ReadLine());
+
         }
     }
 }
